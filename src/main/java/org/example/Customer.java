6294596c7e5100f56e079a67096b0ec34487fe2d
adela <novoad89@osu.cz>
2026-05-28 17:10:19 +0200

    Customer

---
+++ src/main/java/org/example/Customer.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Customer {
+    private String name;
+    private String surname;
+    private String email;
+
+    public Customer(String name, String surname, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
